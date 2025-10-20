f4dd1d2f952007acfb99fc3329115331bd375681
zamanfa <zamanf123@hotmail.co.uk>
2025-10-20 16:15:18 +0100

    VM about to expire

---
+++ FunctionFun/Program.cs
@@ -0,0 +1,19 @@
+﻿static int Adder(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+
+
+static long Adder2(int a, int b) => a + b;
+
+
+
+int result = Adder(4, 7);
+Console.WriteLine(result);
+
+int a = 7;
+int b = 8;
+Console.WriteLine(Adder2(a, b));
+Console.WriteLine(a);
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*NewMethod();
+
+static void NewMethod()
+{
+    int magicNumber = 3;
+    Console.WriteLine($"The magic number is {magicNumber} and twice that is {magicNumber * 2}");
+
+    {
+        string message = "I am hiddent inside my block";
+        Console.WriteLine(message);
+    }
+}*/
+
+
+
+
+
+
+
+
+//string first = @"Hello\nWorld!";
+
+//string second = "Goodbye\nWorld!";
+
+//string message = first + second;
+
+//Console.WriteLine(message);
+
+//bool isSame = first == second;
+
+//Console.WriteLine(isSame);
+
+
+
+//Console.WriteLine("Hello World!");
+
+//char c = 'T';
+
+//bool x = false;
+
+//var t = 32;
+
+//int x = 42;
+
+//float d = 3.14f;
+
+//double y = 3.123432;
+
+
